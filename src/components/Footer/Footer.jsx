650c9d50454fa6654893817c6ce044144bf4c7a1
AKhomyt <AKhomyt@gmail.com>
2022-02-14 17:29:59 +0200

    14.02.2022

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,7 +6,7 @@ export const Footer = () =>{
     //let dispatch = useDispatch()
     return <footer id={s.footer}>
         <div id={s.contacts}>
-            <a href={'https://www.youtube.com/channel/UCTW0FUhT0m-Bqg2trTbSs0g'} target='_blank' rel='noopener noreferrer'>YouYube channel</a>
+            <a href={'https://www.youtube.com/channel/UCTW0FUhT0m-Bqg2trTbSs0g'} target='_blank' rel='noopener noreferrer'>YouTube channel</a>
             <a href='https://social-network.samuraijs.com/' target='_blank' rel='noopener noreferrer'>https://social-network.samuraijs.com/</a>
             <a href={'https://www.youtube.com/watch?v=gb7gMluAeao&list=PLcvhF2Wqh7DNVy1OCUpG3i5lyxyBWhGZ8&index=1'} target='_blank' rel='noopener noreferrer'>YouTube course</a>
         </div>
